8fe7a943703b3368175c71bf49825d327a4726cc
Annino De Petra <andp92@gmail.com>
2021-03-07 16:57:34 +0000

    Add further tests for BST

--- DataStructuresTests/BinarySearchTreeTests.swift
+++ DataStructuresTests/BinarySearchTreeTests.swift
@@ -64,6 +64,57 @@ final class BinarySearchTreeTests: XCTestCase {
         
         XCTAssertEqual(expectedPostorderTraversal, returnedTraversal, "Incorrect Postorder traversal order returned")
     }
+
+    func testBinarySearchTreeBreadthTraversal() {
+        let expectedBreadthTraversal = [50, 30, 70, 20, 40, 60, 10, 52, 55]
+        var returnedTraversal = Array<Int>()
+        
+        tree.traversal(.breadth) {
+            returnedTraversal.append($0.data)
+        }
+        
+        XCTAssertEqual(expectedBreadthTraversal, returnedTraversal, "Incorrect Breadth traversal order returned")
+    }
+    
+    func testEmptyBinarySearchTreeInorderTraversal() {
+        var returnedTraversal = Array<Int>()
+        
+        emptyTree.traversal(.inorder) {
+            returnedTraversal.append($0.data)
+        }
+        
+        XCTAssertTrue(returnedTraversal.isEmpty, "Traversal found element/s in an empty tree")
+    }
+    
+    func testEmptyBinarySearchTreePreorderTraversal() {
+        var returnedTraversal = Array<Int>()
+        
+        emptyTree.traversal(.preorder) {
+            returnedTraversal.append($0.data)
+        }
+        
+        XCTAssertTrue(returnedTraversal.isEmpty, "Traversal found element/s in an empty tree")
+    }
+    
+    func testEmptyBinarySearchTreePostorderTraversal() {
+        var returnedTraversal = Array<Int>()
+        
+        emptyTree.traversal(.postorder) {
+            returnedTraversal.append($0.data)
+        }
+        
+        XCTAssertTrue(returnedTraversal.isEmpty, "Traversal found element/s in an empty tree")
+    }
+
+    func testEmptyBinarySearchTreeBreadthTraversal() {
+        var returnedTraversal = Array<Int>()
+        
+        emptyTree.traversal(.breadth) {
+            returnedTraversal.append($0.data)
+        }
+        
+        XCTAssertTrue(returnedTraversal.isEmpty, "Traversal found element/s in an empty tree")
+    }
     
     func testBinarySearch() {
         let inArrayItems = [20, 55, 70, 40]
@@ -75,11 +126,28 @@ final class BinarySearchTreeTests: XCTestCase {
         notInArrayItems.forEach {
             XCTAssertFalse(tree.search($0), "Item \($0) found in tree")
         }
-        
-        
+    }
+    
+    func testBinarySearchEmptyTree() {
+        let elements = [20, 55, 70, 40]
+        elements.forEach {
+            XCTAssertFalse(emptyTree.search($0), "Item \($0) found in empty tree")
+        }
     }
     
     func testHeightMoreThan0() {
-        
+        XCTAssertEqual(4, tree.height, "Incorrect height returned")
+    }
+    
+    func testEmptyTreeHeight() {
+        XCTAssertEqual(0, emptyTree.height, "Height with no root must return height 0")
+    }
+    
+    func testBinarySearchTreeMinValue() {
+        XCTAssertEqual(tree.min, 10, "Incorrect min value returned")
+    }
+    
+    func testBinarySearchTreeMaxValue() {
+        XCTAssertEqual(tree.max, 70, "Incorrect max value returned")
     }
 }
